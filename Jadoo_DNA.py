3ba11fb15ab510b29f02798b74a5ccde3df5801f
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2020-09-23 15:55:02 +0530

    Update Jadoo_DNA.py

--- Jadoo_DNA.py
+++ Jadoo_DNA.py
@@ -1,25 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
 
 
-@author: saurav
-"""
+
 i=0
+num=0
 ch = input()
 num = len(ch)
 while i<num:
     if ch[i] != "G" and ch[i] !="C" and ch[i] !="T" and ch[i] !="A":
         print("Invalid Input")
+        num=1
         break
-    if ch[i] == "G":
+    if ch[i] == "G" and num==1:
         print('C', end = '')
-    elif ch[i] =="C":
+    elif ch[i] =="C" and num==1:
         print('G', end = '')
-    elif ch[i] =="T":
+    elif ch[i] =="T" and num==1:
         print('A', end = '')
-    elif ch[i] =="A":
-        print('U', end = '') 
-    else:
-        print("Invalid Input")
-        break   
+    elif ch[i] =="A" and num==1:
+        print('U', end = '')   
     i+=1
